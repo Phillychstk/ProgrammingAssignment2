659356b60bb946f8ab1d905c09e55b790165005e
Phillip Hughes <126487279+Phillychstk@users.noreply.github.com>
2023-03-10 07:54:19 -0500

    Add files via upload
    These are my files for programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These will set up to find the inverse of the special matrix.
 
-## Write a short comment describing this function
+## setting invrs which is for range to null
 
 makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  set <- function(y) {
+    x <<- y
+    invrs <<- NULL
+  }
+  get <- function() x
+  setinvrs <- function(inverse) invrs <<- invrs
+  getinvrs <- function() invrs
+  list(set = set,
+       get = get,
+       setinvrs = setinvrs,
+       getinvrs = getinvrs)
 
 }
 
 
-## Write a short comment describing this function
+## this function computes the inverse of the "matrix" if not already input by the previous function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+  invrs <- x$getinvrs()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  dta <- x$get()
+  invrs <- solve(dta, ...)
+  x$setinvrs(invrs)
+  invrs
 }
